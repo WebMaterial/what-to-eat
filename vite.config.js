cf17a220a05375db0894676a0c098be76243d90c
内田直希 <>
2026-02-10 10:21:39 +0900

    fix: remove duplicated Vite config

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,3 @@ export default defineConfig({
 	base: '/what-to-eat/',
 })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
